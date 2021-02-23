0e705200de39c6dbe199949b200d9c9a56ab6ed3
github.nafisstaha <github.nafise.tahaei@gmail.com>
2021-02-22 22:00:03 -0500

    descriptive message here

---
+++ director-details.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+    <!--head-->
+    <head>
+        <meta charset="UTF-8">
+        <title>Publisher Details</title>
+    </head>
+
+    <!--body-->
+    <body>
+
+        <!--form-->
+        <form method="post" action="save-director.php">
+
+            <fieldset>
+                <label for="directorName">Director: </label>
+                <input name="directorName" id="directorName" required maxlength="50"/>
+            </fieldset>
+
+            <button>Save</button>
+        </form>
+
+    </body>
+</html>
